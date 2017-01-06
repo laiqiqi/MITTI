dd56de2356a942a998399271171ebecac0abb5a6
knewRock <who-are-you-._@hotmail.com>
2017-01-06 15:23:59 +0700

    Fix FloatingAIState floating pass player and push AI scene

--- Assets/Scripts/FloatingAIState.cs
+++ Assets/Scripts/FloatingAIState.cs
@@ -42,5 +42,11 @@ public class FloatingAIState : AIState {
 				Random.Range (0f, 10f),
 				enemy.player.transform.position.z + Random.Range (-10f, 10f));
 		}
+		if(Vector3.Distance(enemy.transform.position, enemy.player.transform.position) < 2f){
+			target.position = new Vector3 (enemy.player.transform.position.x + Random.Range (-10f, 10f),
+				//				this.transform.position.y + Random.Range (-10f, 10f),
+				Random.Range (0f, 10f),
+				enemy.player.transform.position.z + Random.Range (-10f, 10f));
+		}
 	}
 }
