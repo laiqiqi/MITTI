19c627a3bd2925ba55d4e227b6148a919332bb9c
knewRock <who-are-you-._@hotmail.com>
2017-01-08 14:12:12 +0700

    Try to commit to change unity version 5.5

---
+++ Assets/Scripts/FloatingAIStateWithNav.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingAIStateWithNav : AIState {
+	private readonly StatePatternAI enemy;
+//	public Transform target;
+	public float speed;
+
+	public FloatingAIStateWithNav(StatePatternAI statePatternAI){
+		enemy = statePatternAI;
+	}
+
+	public void StartState(){
+		enemy.currentState = enemy.floatingState;
+		speed = 10f;
+//		target = new GameObject ().transform;
+		enemy.target.position = new Vector3 (enemy.player.transform.position.x + Random.Range (-10f, 10f),
+			//			this.transform.position.y + Random.Range (-10f, 100f),
+			Random.Range (0f, 10f),
+			enemy.player.transform.position.z + Random.Range (-10f, 10f));
+	}
+
+	public void UpdateState(){
+		Floatiog ();
+		enemy.transform.LookAt(enemy.player.transform);
+	}
+
+	public void EndState(){
+
+	}
+
+	public void StateChangeCondition(){
+	}
+
+	void Floatiog(){
+		float step = speed * Time.deltaTime;
+//		enemy.transform.position = Vector3.MoveTowards(enemy.transform.position, target.position, step);
+		enemy.GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(enemy.target.position);
+		if(Vector3.Distance(enemy.transform.position, enemy.target.position) < 5f){
+			//It is within ~0.1f range, do stuff
+			enemy.target.position = new Vector3 (enemy.player.transform.position.x + Random.Range (-10f, 10f),
+				//				this.transform.position.y + Random.Range (-10f, 10f),
+				Random.Range (0f, 10f),
+				enemy.player.transform.position.z + Random.Range (-10f, 10f));
+		}
+		if(Vector3.Distance(enemy.transform.position, enemy.player.transform.position) < 2f){
+			enemy.target.position = new Vector3 (enemy.player.transform.position.x + Random.Range (-10f, 10f),
+				//				this.transform.position.y + Random.Range (-10f, 10f),
+				Random.Range (0f, 10f),
+				enemy.player.transform.position.z + Random.Range (-10f, 10f));
+		}
+	}
+}
---
+++ Assets/Scripts/SlashAIState.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlashAIState : AIState {
+	private readonly StatePatternAI enemy;
+
+	public SlashAIState(StatePatternAI statePatternAI){
+		enemy = statePatternAI;
+	}
+
+	public void StartState(){
+		enemy.currentState = enemy.slashState;
+	}
+
+	public void UpdateState(){
+		Transform swordTransform = enemy.transform.FindChild("Sword");
+		swordTransform.transform.localPosition = Vector3.Lerp(swordTransform.localPosition, new Vector3(2, 0, 2), 0.1f);
+//		StateChangeCondition ();
+	}
+
+	public void EndState(){
+
+	}
+
+	public void StateChangeCondition(){
+		enemy.floatingState.StartState ();
+	}
+}
