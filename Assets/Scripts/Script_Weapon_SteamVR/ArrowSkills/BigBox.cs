0963cb5e9cd57e7b1cbff4e41b49772cb0613ce2
vasupol11 <vasupol11@yahoo.com>
2017-04-21 04:39:45 +0700

    modify arrow charge no doesnt shoot unless fully charge

---
+++ Assets/Scripts/Script_Weapon_SteamVR/ArrowSkills/BigBox.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBox : MonoBehaviour {
+	private bool CanSlash = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider col){
+		Debug.Log("Big box = trigger enter");
+	}
+	void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Big box = trigger exit");
+		CanSlash = true;
+	}
+	public void SetCanSlash(bool f){
+		CanSlash = f;
+	}
+	public bool GetCanSlash(){
+		return CanSlash;
+	}
+}
