21ed7d555d3653ad6312a8706d60427e0b84a944
vasupol11 <vasupol11@yahoo.com>
2017-04-24 21:23:25 +0700

    Add heal to healing sword

---
+++ Assets/HealFilter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Valve.VR.InteractionSystem;
+
+public class HealFilter : MonoBehaviour {
+
+	private PlayerStat playerStat;
+	private Color color;
+	// Use this for initialization
+	void Start () {
+		playerStat = Player.instance.GetComponent<PlayerStat>();
+		color = this.GetComponent<Image>().material.GetColor("_Color");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Player.instance.GetComponent<PlayerStat>().isHeal) {
+			color.a = 1;
+			this.GetComponent<Image>().material.SetColor("_Color", color);
+			Player.instance.GetComponent<PlayerStat>().isTakeDamage = false;
+		}
+		else {
+			if(this.GetComponent<Image>().material.GetColor("_Color").a > 0){
+				color.a -= 0.2f;
+				this.GetComponent<Image>().material.SetColor("_Color", color);
+			}
+		}
+
+		// color.a = playerStat.health/playerStat.maxHealth;
+		// this.GetComponent<Image>().material.SetColor("_Color", color);
+	}
+}
+
